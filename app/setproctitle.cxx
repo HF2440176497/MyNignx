2665b4e7669af32467fe170fd872fe21e2e6597b
UbuntuServer <wanghf_cust@163.com>
2023-03-13 08:15:51 +0000

    fast

--- app/setproctitle.cxx
+++ app/setproctitle.cxx
@@ -76,7 +76,7 @@ void setproctitle(const char* title) {
     return;
 }
 
-// 释放内存 Not`Fast forward`模式
+// 释放内存 `Fast forward`模式
 void freeresource() {
     delete []g_p_envmem;
     delete []g_p_arglen;
