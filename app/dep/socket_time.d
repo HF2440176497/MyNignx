2d674ca93435b7712251b7b30f729c3c15bd0779
UbuntuServer <wanghf_cust@163.com>
2023-06-01 01:36:02 +0000

    Model "TimerThread"

---
+++ app/dep/socket_time.d
@@ -0,0 +1,12 @@
+/home/wanghf/Project/Ch6/app/link_obj/socket_time.o: socket_time.cxx /home/wanghf/Project/Ch6/_include/func.h \
+ /home/wanghf/Project/Ch6/_include/c_lock.h \
+ /home/wanghf/Project/Ch6/_include/c_socket.h \
+ /home/wanghf/Project/Ch6/_include/comm.h \
+ /home/wanghf/Project/Ch6/_include/comm.h \
+ /home/wanghf/Project/Ch6/_include/macro.h \
+ /home/wanghf/Project/Ch6/_include/global.h \
+ /home/wanghf/Project/Ch6/_include/c_memory.h \
+ /home/wanghf/Project/Ch6/_include/c_threadpool.h \
+ /home/wanghf/Project/Ch6/_include/c_socketlogic.h \
+ /home/wanghf/Project/Ch6/_include/c_socket.h \
+ /home/wanghf/Project/Ch6/_include/c_socketlogic.h
